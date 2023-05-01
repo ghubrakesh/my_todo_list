414f34038facc6c030351947a88a096214b5dd26
ghubrakesh <sabalerakesh143@gmail.com>
2023-05-02 01:44:35 +0530

    edits for vercel

--- todolist_project/todolist_project/settings.py
+++ todolist_project/todolist_project/settings.py
@@ -139,5 +139,5 @@ LOGIN_URL = 'login'
 
 #static work 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
