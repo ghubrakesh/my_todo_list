069cbe2f197c5f5f674467f571254d0f6ceb76a0
ghubrakesh <sabalerakesh143@gmail.com>
2023-05-01 16:42:29 +0530

    updated error logs

--- todolist_project/todolist_project/urls.py
+++ todolist_project/todolist_project/urls.py
@@ -1,5 +1,4 @@
-"""todolist_project URL Configuration
-
+"""todolistprj URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -16,15 +15,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('todo.urls')),
-
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('', include('todo.urls'))
 ]
--- todolist_project/todolist_project/wsgi.py
+++ todolist_project/todolist_project/wsgi.py
@@ -1,8 +1,6 @@
 """
 WSGI config for todolist_project project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
@@ -14,5 +12,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist_project.settings')
 
 application = get_wsgi_application()
-
-app = application
