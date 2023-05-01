3cb7a35574a7f902f9a594fb84030a60c49f53ae
ghubrakesh <sabalerakesh143@gmail.com>
2023-05-02 00:51:26 +0530

    migrated to postgres

--- todolist_project/todolist_project/settings.py
+++ todolist_project/todolist_project/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'todolist_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'w8YOUrSS3h5eq8wEGLrd',
+        'HOST': 'containers-us-west-101.railway.app',
+        'PORT': '7234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
