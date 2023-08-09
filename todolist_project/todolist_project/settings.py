d1686c649138e37694000eadffb0dac89857a3c9
Rakesh Sabale <102187286+ghubrakesh@users.noreply.github.com>
2023-08-09 20:42:41 +0530

    Update settings.py
    allowed all hosts to deploy

--- todolist_project/todolist_project/settings.py
+++ todolist_project/todolist_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(*9h0-%=bofhr=32-t%-8v02s!9u7vj=8#c$56cibb-dkogii(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
